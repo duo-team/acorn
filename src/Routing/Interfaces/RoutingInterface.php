80f0bb1430251c473db82a73ac17477dd3ad3b0a
Kacper Pruszyński <kacper.pruszynski99@gmail.com>
2021-07-09 22:05:46 +0200

    Add interface method

--- src/Routing/Interfaces/RoutingInterface.php
+++ src/Routing/Interfaces/RoutingInterface.php
@@ -10,4 +10,14 @@ interface RoutingInterface
      * @return void
      */
     public function registerRoutes(): void;
+
+    /**
+     * Make at handler signature.
+     *
+     * @param string $class
+     * @param string $method
+     *
+     * @return string
+     */
+    public function atHandler(string $class, string $method): string;
 }
