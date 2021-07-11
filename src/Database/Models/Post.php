ed1bd52a7946d43b0cce8a08d8ef07ef5d086182
Kacper Pruszyński <kacper.pruszynski99@gmail.com>
2021-07-11 17:19:05 +0200

    Improve database post model

---
+++ src/Database/Models/Post.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace DuoTeam\Acorn\Database\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use DuoTeam\Acorn\Database\Support;
+use Illuminate\Support\Carbon;
+
+/**
+ * @property int $ID
+ * @property int $post_author
+ * @property Carbon $post_date
+ * @property Carbon $post_date_gmt
+ * @property string $post_content
+ * @property string $post_title
+ * @property string $post_excerpt
+ * @property string $post_status
+ * @property string $comment_status
+ * @property string $ping_status
+ * @property string $post_password
+ * @property string $post_name
+ * @property string $to_ping
+ * @property string $pinged
+ * @property Carbon $post_modified
+ * @property Carbon $post_modified_gmt
+ * @property string $post_content_filtered
+ * @property int $post_parent
+ * @property string $guid
+ * @property int $menu_order
+ * @property string $post_type
+ * @property string $post_mime_type
+ * @property int $comment_count
+ */
+class Post extends Model
+{
+    use Support\HasTimestamps;
+
+    /**
+     * The name of the "created at" column.
+     *
+     * @var string|null
+     */
+    const CREATED_AT = 'post_date';
+
+    /**
+     * The name of the "created at" column in GMT.
+     *
+     * @var string|null
+     */
+    const CREATED_AT_GMT = 'post_date_gmt';
+
+    /**
+     * The name of the "updated at" column.
+     *
+     * @var string|null
+     */
+    const UPDATED_AT = 'post_modified';
+
+    /**
+     * The name of the "updated at" column in GMT.
+     *
+     * @var string|null
+     */
+    const UPDATED_AT_GMT = 'post_modified_gmt';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'ID';
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'post_date' => 'date',
+        'post_date_gmt' => 'date',
+        'post_modified' => 'date',
+        'post_modified_gmt' => 'date',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'post_author',
+        'post_date',
+        'post_date_gmt',
+        'post_content',
+        'post_title',
+        'post_excerpt',
+        'post_status',
+        'post_password',
+        'post_name',
+        'to_ping',
+        'pinged',
+        'post_modified',
+        'post_modified_gmt',
+        'post_content_filtered',
+        'post_parent',
+        'guid',
+        'menu_order',
+        'post_type',
+        'post_mime_type',
+        'comment_count'
+    ];
+
+    /**
+     * Set the value of the "created at" attribute.
+     *
+     * @param mixed $value
+     * @return $this
+     */
+    public function setCreatedAt($value)
+    {
+        parent::setCreatedAt($value);
+        $this->{$this->getCreatedAtGmtColumn()} = Carbon::parse($value)->utc();
+
+        return $this;
+    }
+
+    /**
+     * Set the value of the "updated at" attribute.
+     *
+     * @param mixed $value
+     * @return $this
+     */
+    public function setUpdatedAt($value)
+    {
+        parent::setUpdatedAt($value);
+        $this->{$this->getUpdatedAtGmtColumn()} = Carbon::parse($value)->utc();
+
+        return $this;
+    }
+}
---
+++ src/Database/Support/HasTimestamps.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace DuoTeam\Acorn\Database\Support;
+
+trait HasTimestamps
+{
+    /**
+     * Get the name of the "created at" column.
+     *
+     * @return string|null
+     */
+    public function getCreatedAtGmtColumn(): ?string
+    {
+        return static::CREATED_AT_GMT;
+    }
+
+    /**
+     * Get the name of the "updated at" column.
+     *
+     * @return string|null
+     */
+    public function getUpdatedAtGmtColumn(): ?string
+    {
+        return static::UPDATED_AT_GMT;
+    }
+
+    /**
+     * Get the fully qualified "created at" column.
+     *
+     * @return string
+     */
+    public function getQualifiedCreatedAtGmtColumn(): string
+    {
+        return $this->qualifyColumn($this->getCreatedAtGmtColumn());
+    }
+
+    /**
+     * Get the fully qualified "updated at" column.
+     *
+     * @return string
+     */
+    public function getQualifiedUpdatedAtGmtColumn(): string
+    {
+        return $this->qualifyColumn($this->getUpdatedAtGmtColumn());
+    }
+}
