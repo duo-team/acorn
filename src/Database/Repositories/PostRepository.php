5a77a2fc7d74c94a502fac427c8a3ea4c39402c6
Kacper Pruszyński <kacper.pruszynski99@gmail.com>
2021-07-11 18:09:11 +0200

    Create repositories

---
+++ src/Database/Repositories/PostRepository.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace DuoTeam\Acorn\Database\Repositories;
+
+use App\Models\Post;
+use DuoTeam\Acorn\Database\Repository;
+use DuoTeam\Acorn\Enums\PostCommentStatusEnum;
+use DuoTeam\Acorn\Enums\PostPingStatus;
+use DuoTeam\Acorn\Enums\PostStatusEnum;
+use DuoTeam\Acorn\Enums\PostTypeEnum;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+class PostRepository extends Repository
+{
+    /**
+     * Post model instance.
+     *
+     * @var Post
+     */
+    private $model;
+
+    /**
+     * @param Post $model
+     */
+    public function __construct(Post $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * Get model instance.
+     *
+     * @return Model
+     */
+    public function model(): Model
+    {
+        return $this->model;
+    }
+
+    /**
+     * Get model default attributes values.
+     *
+     * @return array
+     */
+    public function defaultAttributes(): array
+    {
+        return [
+            'post_content' => '',
+            'post_title' => '',
+            'post_excerpt' => '',
+            'post_status' => PostStatusEnum::PRIVATE(),
+            'comment_status' => PostCommentStatusEnum::CLOSED(),
+            'ping_status' => PostPingStatus::CLOSED(),
+        ];
+    }
+
+    /**
+     * Get all models.
+     *
+     * @return Collection
+     */
+    public function all(): Collection
+    {
+        return $this->builder()
+            ->where('post_type', '=', PostTypeEnum::POST())
+            ->get();
+    }
+}
---
+++ src/Database/Repository.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace DuoTeam\Acorn\Database;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\QueryException;
+use RuntimeException;
+
+abstract class Repository
+{
+    /**
+     * Get model instance.
+     *
+     * @return Model
+     */
+    abstract public function model(): Model;
+
+    /**
+     * Get eloquent builder instance.
+     *
+     * @return Builder
+     */
+    public function builder(): Builder
+    {
+        return $this->model()->newQuery();
+    }
+
+    /**
+     * Get model default attributes values.
+     *
+     * @return array
+     */
+    public function defaultAttributes(): array
+    {
+        return [];
+    }
+
+    /**
+     * Create model.
+     *
+     * @param array $attributes
+     *
+     * @return Model
+     */
+    public function create(array $attributes): Model
+    {
+        $modelId = wp_insert_post($this->prepareAttributes($attributes));
+
+        if (is_wp_error($modelId)) {
+            throw new RuntimeException($modelId->get_error_message());
+        }
+
+        return $this->get($modelId);
+    }
+
+    /**
+     * Update model and return updated.
+     *
+     * @param string $id
+     * @param array $attributes
+     *
+     * @return Model
+     */
+    public function update(string $id, array $attributes): Model
+    {
+        $model = $this->get($id);
+        $model->update($this->prepareAttributes($attributes));
+
+        return $model->refresh();
+    }
+
+    /**
+     * Get model with id or fail.
+     *
+     * @param string $id
+     * @param string[] $columns
+     *
+     * @return Model
+     */
+    public function get(string $id, array $columns = ['*']): Model
+    {
+        return $this->builder()
+            ->where($this->model()->getKeyName(), '=', $id)
+            ->firstOrFail($columns);
+    }
+
+    /**
+     * Get model with id or return null.
+     *
+     * @param string $id
+     * @param string[] $columns
+     *
+     * @return Model
+     */
+    public function find(string $id, array $columns = ['*']): ?Model
+    {
+        return $this->builder()
+            ->where($this->model()->getKeyName(), '=', $id)
+            ->first($columns);
+    }
+
+    /**
+     * Get all models.
+     *
+     * @return Collection
+     */
+    public function all(): Collection
+    {
+        return $this->builder()->get();
+    }
+
+    /**
+     * Prepare attributes. Merge with defaults.
+     *
+     * @param array $attributes
+     *
+     * @return array
+     */
+    public function prepareAttributes(array $attributes): array
+    {
+        return array_merge($this->defaultAttributes(), $attributes);
+    }
+}
