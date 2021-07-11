07c7ebed45a250cdecd28cae9af1a9ce62f794a2
Kacper Pruszyński <kacper.pruszynski99@gmail.com>
2021-07-11 18:12:15 +0200

    Add misiing enum class

--- src/Enums/PostCommentStatusEnum.php
+++ src/Enums/PostCommentStatusEnum.php
@@ -3,11 +3,13 @@
 
 namespace DuoTeam\Acorn\Enums;
 
+use MyCLabs\Enum\Enum;
+
 /**
  * @method static PostCommentStatusEnum OPEN()
  * @method static PostCommentStatusEnum CLOSED()
  */
-class PostCommentStatusEnum
+class PostCommentStatusEnum extends Enum
 {
     protected const OPEN = 'open';
     protected const CLOSED = 'closed';
--- src/Enums/PostPingStatus.php
+++ src/Enums/PostPingStatus.php
@@ -3,11 +3,13 @@
 
 namespace DuoTeam\Acorn\Enums;
 
+use MyCLabs\Enum\Enum;
+
 /**
  * @method static PostPingStatus OPEN()
  * @method static PostPingStatus CLOSED()
  */
-class PostPingStatus
+class PostPingStatus extends Enum
 {
     protected const OPEN = 'open';
     protected const CLOSED = 'closed';
