02f0194c4dd2b1e859fc4aaeab30e451ee954ad1
Kacper Pruszyński <kacper.pruszynski99@gmail.com>
2021-07-13 23:19:15 +0200

    add post meta model

---
+++ src/Database/Models/PostMeta.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace DuoTeam\Acorn\Database\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * @property int $meta_id
+ * @property int $post_id
+ * @property string $meta_key
+ * @property mixed $meta_value
+ *
+ * @property Post $post
+ */
+class PostMeta extends Model
+{
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'meta_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'post_id',
+        'meta_key',
+        'meta_value',
+    ];
+
+    /**
+     * Get related post.
+     *
+     * @return BelongsTo
+     */
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class, 'post_id', 'ID');
+    }
+}
