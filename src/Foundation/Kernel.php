46148ad3682ac3c81e2465f22c4a1ff7bd2c061c
Kacper Pruszyński <kacper.pruszynski99@gmail.com>
2021-07-14 21:02:49 +0200

    remove bindings replacing

--- src/Foundation/Kernel.php
+++ src/Foundation/Kernel.php
@@ -5,40 +5,11 @@ namespace DuoTeam\Acorn\Foundation;
 use DuoTeam\Acorn\Bootstrap\ApplyFilters;
 use DuoTeam\Acorn\Bootstrap\HandleExceptions;
 use DuoTeam\Acorn\Bootstrap\CaptureRequest;
-use DuoTeam\Acorn\Console\Kernel as ConsoleKernel;
-use DuoTeam\Acorn\Exceptions\Handler;
-use Roots\Acorn\Application;
 use Roots\Acorn\Bootloader as RootsKernel;
-use Illuminate\Contracts\Debug\ExceptionHandler as HandlerContract;
 use Roots\Acorn\Bootstrap\RegisterGlobals;
-use Roots\Acorn\Console\Kernel as RootsConsoleKernel;
 
 class Kernel extends RootsKernel
 {
-    /**
-     * Bindings to replace.
-     *
-     * @var string[]
-     */
-    protected $bindingsToReplace = [
-        HandlerContract::class => Handler::class,
-        RootsConsoleKernel::class => ConsoleKernel::class
-    ];
-
-    /**
-     * Replace default bindings with DuoTeam.
-     *
-     * @return void
-     */
-    protected function replaceBindings(): void
-    {
-        $this->queue[] = function (Application $app) {
-            foreach ($this->bindingsToReplace as $abstract => $concrete) {
-                $app->singleton($abstract, $concrete);
-            }
-        };
-    }
-
     /**
      * Get the list of application bootstraps
      *
@@ -46,7 +17,6 @@ class Kernel extends RootsKernel
      */
     protected function bootstrap(): array
     {
-        $this->replaceBindings();
         return apply_filters('acorn/bootstrap', $this->bootstraps());
     }
 
