413a591e4fe2c0c011f28726bc4cee96f675f631
Kacper Pruszyński <kacper.pruszynski99@gmail.com>
2021-07-14 21:02:29 +0200

    load deferred providers to load commands in good way

--- src/Console/Kernel.php
+++ src/Console/Kernel.php
@@ -41,34 +41,18 @@ class Kernel extends ConsoleKernel
     }
 
     /**
-     * Get illuminate commands to register.
-     *
-     * @return array
-     */
-    protected function illuminateCommands(): array
-    {
-        return array_filter([
-            'Illuminate\Database\Console\Migrations\FreshCommand',
-            'Illuminate\Database\Console\Migrations\InstallCommand',
-            'Illuminate\Database\Console\Migrations\MigrateCommand',
-            'Illuminate\Database\Console\Migrations\MigrateMakeCommand',
-            'Illuminate\Database\Console\Migrations\RefreshCommand',
-            'Illuminate\Database\Console\Migrations\ResetCommand',
-            'Illuminate\Database\Console\Migrations\RollbackCommand',
-            'Illuminate\Database\Console\Migrations\StatusCommand',
-        ], 'class_exists');
-    }
-
-    /**
      * Get the Console application instance.
      *
      * @return Application
      */
     protected function getConsole(): Application
     {
-        return parent::getConsole()
-            ->resolveCommands($this->rootsCommands())
-            ->resolveCommands($this->illuminateCommands());
+        $console = parent::getConsole();
+        $console->resolveCommands($this->rootsCommands());
+
+        $this->app->loadDeferredProviders();
+
+        return $console;
     }
 
     /**
