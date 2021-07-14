843cf811c42bef8df75a437699bfac3968e74b29
Kacper Pruszyński <kacper.pruszynski99@gmail.com>
2021-07-14 20:01:31 +0200

    improve kernel

--- src/Foundation/Kernel.php
+++ src/Foundation/Kernel.php
@@ -5,10 +5,39 @@ namespace DuoTeam\Acorn\Foundation;
 use DuoTeam\Acorn\Bootstrap\ApplyFilters;
 use DuoTeam\Acorn\Bootstrap\HandleExceptions;
 use DuoTeam\Acorn\Bootstrap\CaptureRequest;
+use DuoTeam\Acorn\Console\Kernel as ConsoleKernel;
+use DuoTeam\Acorn\Exceptions\Handler;
+use Roots\Acorn\Application;
 use Roots\Acorn\Bootloader as RootsKernel;
+use Illuminate\Contracts\Debug\ExceptionHandler as HandlerContract;
+use Roots\Acorn\Console\Kernel as RootsConsoleKernel;
 
 class Kernel extends RootsKernel
 {
+    /**
+     * Bindings to replace.
+     *
+     * @var string[]
+     */
+    protected $bindingsToReplace = [
+        HandlerContract::class => Handler::class,
+        RootsConsoleKernel::class => ConsoleKernel::class
+    ];
+
+    /**
+     * Replace default bindings with DuoTeam.
+     *
+     * @return void
+     */
+    protected function replaceBindings(): void
+    {
+        $this->call(function (Application $app) {
+            foreach ($this->bindingsToReplace as $abstract => $concrete) {
+                $app->singleton($abstract, $concrete);
+            }
+        });
+    }
+
     /**
      * Get the list of application bootstraps
      *
@@ -16,11 +45,22 @@ class Kernel extends RootsKernel
      */
     protected function bootstrap(): array
     {
-        return apply_filters('acorn/bootstrap', array_merge([
+        $this->replaceBindings();
+        return apply_filters('acorn/bootstrap', $this->bootstraps());
+    }
+
+    /**
+     * Get bootstrapping classes.
+     *
+     * @return array
+     */
+    protected function bootstraps(): array
+    {
+        return array_merge([
             CaptureRequest::class,
         ], parent::bootstrap(), [
             ApplyFilters::class,
             HandleExceptions::class
-        ]));
+        ]);
     }
 }
