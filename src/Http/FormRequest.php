2297e9a9dd826fa694d051ef6f9c407e208bf658
Kacper Pruszyński <kacper.pruszynski99@gmail.com>
2021-07-18 16:01:58 +0200

    use illuminate form request

--- src/Http/FormRequest.php
+++ src/Http/FormRequest.php
@@ -3,126 +3,16 @@
 namespace DuoTeam\Acorn\Http;
 
 use DuoTeam\Acorn\Exceptions\Http\AuthorizationException;
-use Illuminate\Contracts\Container\BindingResolutionException;
-use Illuminate\Contracts\Container\Container;
-use Illuminate\Contracts\Validation\Factory as ValidationFactory;
-use Illuminate\Contracts\Validation\ValidatesWhenResolved;
-use Illuminate\Contracts\Validation\Validator;
-use Illuminate\Http\Request;
-use Illuminate\Validation\ValidatesWhenResolvedTrait;
-use Illuminate\Validation\ValidationException;
+use Illuminate\Foundation\Http\FormRequest as Request;
 
-class FormRequest extends Request implements ValidatesWhenResolved
+abstract class FormRequest extends Request
 {
-    use ValidatesWhenResolvedTrait;
-
-    /**
-     * The container instance.
-     *
-     * @var Container
-     */
-    protected $container;
-
-    /**
-     * The key to be used for the view error bag.
-     *
-     * @var string
-     */
-    protected $errorBag = 'default';
-
-    /**
-     * The validator instance.
-     *
-     * @var Validator
-     */
-    protected $validator;
-
-    /**
-     * Get the validator instance for the request.
-     *
-     * @return Validator
-     * @throws BindingResolutionException
-     */
-    protected function getValidatorInstance()
-    {
-        if ($this->validator) {
-            return $this->validator;
-        }
-
-        $factory = $this->container->make(ValidationFactory::class);
-
-        if (method_exists($this, 'validator')) {
-            $validator = $this->container->call([$this, 'validator'], compact('factory'));
-        } else {
-            $validator = $this->createDefaultValidator($factory);
-        }
-
-        if (method_exists($this, 'withValidator')) {
-            $this->withValidator($validator);
-        }
-
-        $this->setValidator($validator);
-
-        return $this->validator;
-    }
-
-    /**
-     * Create the default validator instance.
-     *
-     * @param ValidationFactory $factory
-     * @return Validator
-     */
-    protected function createDefaultValidator(ValidationFactory $factory)
-    {
-        return $factory->make(
-            $this->validationData(),
-            $this->container->call([$this, 'rules']),
-            $this->messages(),
-            $this->attributes()
-        );
-    }
-
-    /**
-     * Get data to be validated from the request.
-     *
-     * @return array
-     */
-    public function validationData()
-    {
-        return $this->all();
-    }
-
-    /**
-     * Handle a failed validation attempt.
-     *
-     * @param Validator $validator
-     * @return void
-     *
-     * @throws ValidationException
-     */
-    protected function failedValidation(Validator $validator)
-    {
-        throw (new ValidationException($validator))->errorBag($this->errorBag);
-    }
-
-    /**
-     * Determine if the request passes the authorization check.
-     *
-     * @return bool
-     */
-    protected function passesAuthorization()
-    {
-        if (method_exists($this, 'authorize')) {
-            return $this->container->call([$this, 'authorize']);
-        }
-
-        return true;
-    }
-
     /**
      * Handle a failed authorization attempt.
      *
      * @return void
+     *
+     * @throws AuthorizationException
      */
     protected function failedAuthorization()
     {
@@ -130,58 +20,12 @@ class FormRequest extends Request implements ValidatesWhenResolved
     }
 
     /**
-     * Get the validated data from the request.
+     * Get the URL to redirect to on a validation error.
      *
-     * @return array
+     * @return string
      */
-    public function validated()
+    protected function getRedirectUrl(): string
     {
-        return $this->validator->validated();
-    }
-
-    /**
-     * Get custom messages for validator errors.
-     *
-     * @return array
-     */
-    public function messages()
-    {
-        return [];
-    }
-
-    /**
-     * Get custom attributes for validator errors.
-     *
-     * @return array
-     */
-    public function attributes()
-    {
-        return [];
-    }
-
-    /**
-     * Set the Validator instance.
-     *
-     * @param Validator $validator
-     * @return $this
-     */
-    public function setValidator(Validator $validator)
-    {
-        $this->validator = $validator;
-
-        return $this;
-    }
-
-    /**
-     * Set the container implementation.
-     *
-     * @param Container $container
-     * @return $this
-     */
-    public function setContainer(Container $container)
-    {
-        $this->container = $container;
-
-        return $this;
+        return get_home_url();
     }
 }
