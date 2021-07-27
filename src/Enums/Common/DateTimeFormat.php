c72d4cc0ce4ecca16a183e60a1f26721afa6f150
Kacper Pruszyński <kacper.pruszynski99@gmail.com>
2021-07-27 20:13:26 +0200

    change datetime const

--- src/Enums/Common/DateTimeFormat.php
+++ src/Enums/Common/DateTimeFormat.php
@@ -6,12 +6,12 @@ use MyCLabs\Enum\Enum;
 
 /**
  * @method static DateTimeFormat DATETIME()
- * @method static DateTimeFormat DATE()
- * @method static DateTimeFormat TIME()
+ * @method static DateTimeFormat FULL_DATE()
+ * @method static DateTimeFormat FULL_TIME()
  */
 class DateTimeFormat extends Enum
 {
     private const DATETIME = 'Y-m-d H:i:s';
-    private const DATE = 'Y-m-d';
-    private const TIME = 'H:i:s';
+    private const FULL_DATE = 'Y-m-d';
+    private const FULL_TIME = 'H:i:s';
 }
