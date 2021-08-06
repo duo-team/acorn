0acd485f32fc515bbb405c1e80c3883a5054203f
Kacper Pruszyński <kacper.pruszynski99@gmail.com>
2021-08-06 21:30:53 +0200

    change filters config

--- config/filters.php
+++ config/filters.php
@@ -4,7 +4,8 @@ return [
     /**
      * DuoTeam filters...
      */
-    DuoTeam\Acorn\Filters\Theme\Acf\JsonPath::class,
+    DuoTeam\Acorn\Filters\Theme\Acf\SaveJsonPath::class,
+    DuoTeam\Acorn\Filters\Theme\Acf\LoadJsonPath::class,
     DuoTeam\Acorn\Filters\Theme\Acf\AddOptions::class,
     DuoTeam\Acorn\Filters\Theme\Support\I18n::class,
 
